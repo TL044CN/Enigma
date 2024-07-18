3da719b69157a3cce04848aa4e1a82324fdd51d4
TL044CN <gunnisan2@web.de>
2024-07-18 10:52:39 +0200

    INI: Initialized Repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# Enigma
+
+[![Build Status](https://jenkins.shodan.fyi/buildStatus/icon?job=Enigma)](https://jenkins.shodan.fyi/job/Enigma/)
+A simple Application Engine
+
---
+++ include/Enigma.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Enigma.hpp
+ * @author TL044CN
+ * @brief Include file for the Enigma Engine
+ * @version 0.1
+ * @date 2024-07-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#pragma once
+
